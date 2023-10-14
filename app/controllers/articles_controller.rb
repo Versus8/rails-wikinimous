3901181e52754c6bf453e968f3fb2506d5bf5084
Veronique Tureau <veronique.tureau@gmail.com>
2023-10-14 17:36:46 -0400

    articles added

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+end
